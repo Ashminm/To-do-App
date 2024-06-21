83a8511de53ea4afc7792d1ffb34f424a5e6ee09
Ashmin <ashmin2802@gmail.com>
2024-06-21 20:56:47 +0530

    remove old function

--- src/component/TodoApp.jsx
+++ src/component/TodoApp.jsx
@@ -1,8 +1,6 @@
 import React, { useState,useEffect } from "react";
 
 function TodoApp() {
-    const [getitem, setGetItem] = useState([]);
-    const [getType, setGetType] = useState("");
     const [date,setDate]=useState(new Date())
    
     useEffect(()=>{
@@ -23,8 +21,6 @@ function TodoApp() {
                             className="form-control p-3 "
                             id="Transparant"
                             style={{ borderRadius: "30px" }}
-                            value={getType}
-                            onChange={(e) => setGetType(e.target.value)}
                         />
                         <button
                             className="btn p-3 ps-5 pe-5 bg-dark text-light"
@@ -35,38 +31,24 @@ function TodoApp() {
                                 outline: "none",
                                 border: "1px solid #000",
                             }}
-                            onClick={() => setGetItem([...getitem, {id:Date.now(),text:getType,status:false}])}
                         >
                             add
                         </button>
                     </div>
                 </div>
                 <div>
-                    {
-                        getitem ? getitem.map((item)=>(
+                   
                             <div className="shadow d-flex align-items-center rounded mt-4 p-3">
                         <div className="d-flex align-items-center">
-                            <input type="checkbox" onChange={(e)=>{console.log(e.target.checked);console.log(item);
-                            setGetItem(getitem.filter(values=>{
-                                if(values.id===item.id){
-                                    values.status=e.target.checked
-                                }
-                                return values
-                            }))
-                            
-                            }} value={item.status} className="styled-checkbox" id="myCheckbox" />
+                            <input type="checkbox"  className="styled-checkbox" id="myCheckbox" />
                         </div>
                         <div className="m-0 ps-3 w-100 text-start">
-                            <p className="m-0">{item.text}</p>
+                            <p className="m-0"></p>
                         </div>
                         <div className="text-end pe-2 ps-2">
                             <i className="fa-regular fa-circle-xmark"></i>
                         </div>
                     </div>
-                        )):(
-                            <p className="text-danger">no items</p>
-                        )
-                    }
                 </div>
             </div>
         </div>
