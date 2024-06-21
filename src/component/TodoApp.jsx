023ae7faa72c9213619b86744c3fc1042c646b5d
Ashmin <ashmin2802@gmail.com>
2024-06-21 22:39:27 +0530

    add new function

--- src/component/TodoApp.jsx
+++ src/component/TodoApp.jsx
@@ -2,25 +2,63 @@ import React, { useState,useEffect } from "react";
 
 function TodoApp() {
     const [date,setDate]=useState(new Date())
+    const [task,setTask]=useState([
+        {title:"Example Task",completed:false}
+    ])
+    const [value,setValue]=useState("")
    
+    const handleAdd=()=>{
+        // console.log(value);
+        if (value.trim()) { 
+            const newTasks = [...task, { title: value ,completed:false}];
+            setTask(newTasks);
+            setValue(""); 
+        } else {
+            console.log("Please add a task");
+        }
+        
+    }
+    const handleremove=(index)=>{
+        const newDelete = [...task]
+        newDelete.splice(index,1);
+        setTask(newDelete)
+        console.log("task deleted!!");
+    }
+
+    const handleCompleted = (index) => {
+        const newTasks = task.map((taske, i) =>
+            i === index ? { ...taske, completed: !taske.completed } : taske
+        );
+        setTask(newTasks);
+        console.log("Completed!!");
+    };
+
+    const numberOfTasks = task.length > 0 ? task.length - 1 : 0;
+
     useEffect(()=>{
-        setInterval(()=>setDate(new Date()),1000)
+        const intervalId = setInterval(() => setDate(new Date()), 1000);
+    return () => clearInterval(intervalId);
     },[])
-    // console.log(date);
+
     return (
-        <div>
+      
             <div className="todo-container">
                 <div className="p-5 shadow" style={{ borderRadius: "18px", backgroundColor: "#38ef7ece"}}>
-                    <h2 className="pb-4 m-0 text-start">
+                    <div className="d-flex align-items-center justify-content-between">
+                    <h3 className="pb-4 m-0 text-start">
                         TODAY<i className="fa-solid fa-minus"></i> {date.toLocaleDateString()}
-                    </h2>
+                    </h3>
+                    <p><span className="text-danger h4">{numberOfTasks}</span> Task Pending</p>
+                    </div>
                     <div className="d-flex align-items-center">
                         <input
                             type="text"
-                            placeholder="Enter Your Task"
+                            placeholder="Enter Your New Task"
                             className="form-control p-3 "
                             id="Transparant"
                             style={{ borderRadius: "30px" }}
+                            value={value}
+                          onChange={(e)=>setValue(e.target.value)}    
                         />
                         <button
                             className="btn p-3 ps-5 pe-5 bg-dark text-light"
@@ -31,27 +69,36 @@ function TodoApp() {
                                 outline: "none",
                                 border: "1px solid #000",
                             }}
+                            onClick={handleAdd}
                         >
                             add
                         </button>
                     </div>
                 </div>
                 <div>
-                   
-                            <div className="shadow d-flex align-items-center rounded mt-4 p-3">
+                   {
+                    task.map((i,index)=>(
+                        <div className="shadow d-flex align-items-center rounded mt-4 p-3" key={index}>
                         <div className="d-flex align-items-center">
-                            <input type="checkbox"  className="styled-checkbox" id="myCheckbox" />
+                        <input
+                                type="checkbox"
+                                className="styled-checkbox"
+                                id={`myCheckbox${index}`}
+                                checked={task.completed}
+                                onChange={() => handleCompleted(index)}
+                            />
                         </div>
                         <div className="m-0 ps-3 w-100 text-start">
-                            <p className="m-0"></p>
+                            <p className="m-0" style={{ textDecoration: task.completed ? "line-through" : "none" }}>{i.title}</p>
                         </div>
                         <div className="text-end pe-2 ps-2">
-                            <i className="fa-regular fa-circle-xmark"></i>
+                            <i className="fa-regular fa-circle-xmark h4 m-0 text-danger" onClick={()=>handleremove(index)}></i>
                         </div>
                     </div>
+                    ))
+                   }
                 </div>
             </div>
-        </div>
     );
 }
 
